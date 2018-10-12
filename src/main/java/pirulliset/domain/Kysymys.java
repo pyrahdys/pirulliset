af6a4e193db813f6d2a5eb29e10ffe5c5675cf84
user1 <niko.latvakoski@gmail.com>
2018-10-12 18:07:13 +0300

    Kysymyksille oma luokka

--- src/main/java/pirulliset/domain/Kysymys.java
+++ src/main/java/pirulliset/domain/Kysymys.java
@@ -1,10 +1,27 @@
 
 package pirulliset.domain;
 
-/**
- *
- * @author pyrahdys
- */
 public class Kysymys {
     
+    String kurssi;
+    String aihe;
+    String kysymysteksti;
+
+    public Kysymys(String kurssi, String aihe, String kysymysteksti) {
+        this.kurssi = kurssi;
+        this.aihe = aihe;
+        this.kysymysteksti = kysymysteksti;
+    }
+
+    public String getAihe() {
+        return aihe;
+    }
+
+    public String getKurssi() {
+        return kurssi;
+    }
+
+    public String getKysymysteksti() {
+        return kysymysteksti;
+    }
 }
